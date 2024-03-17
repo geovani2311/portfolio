4a05a5df5129102e260d9976d243df9591d3b96e
Geovani Lima <122645609+geovani2311@users.noreply.github.com>
2024-03-17 14:08:47 -0300

    Update 🎲_Some_projects.py

--- pages/🎲_Some_projects.py
+++ pages/🎲_Some_projects.py
@@ -1,7 +1,31 @@
 import streamlit as st
 
+st.set_page_config(layout="wide")
+
 col1, col2 = st.columns(2)
 with col1:
-    st.subheader("Pokemon dashboard in excel")
+    st.subheader("Project about analyse about prices of gasoline in Brazil")
+    st.text("Cleaning and preprocessing the data")
+    st.link_button('https://github.com/geovani2311/Pandas_Exercises/blob/main/exercicios-pandas.ipynb','https://github.com/geovani2311/Pandas_Exercises/blob/main/exercicios-pandas.ipynb')
 with col2:
-    st.image('project_1.png')
+    st.image('img/gasoline.jpg',width=250)
+
+st.subheader(" ", divider='gray')
+
+col1, col2 = st.columns(2)
+with col1:
+    st.subheader("Project about analyse about Obesity among adults by country, 1975-2016")
+    st.text("Cleaning and preprocessing the data")
+    st.link_button('https://github.com/geovani2311/Pandas_Exercises/blob/main/projeto-analise-de-dados.ipynb','https://github.com/geovani2311/Pandas_Exercises/blob/main/projeto-analise-de-dados.ipynb')
+with col2:
+    st.image('img/obesidity.jpg',width=250)
+
+st.subheader(" ", divider='gray')
+
+col1, col2 = st.columns(2)
+with col1:
+    st.subheader("Project about analyse about Obesity among adults by country, 1975-2016")
+    st.text("Cleaning and preprocessing the data")
+    st.link_button('https://github.com/geovani2311/Pandas_Exercises/blob/main/projeto-analise-de-dados.ipynb','https://github.com/geovani2311/Pandas_Exercises/blob/main/projeto-analise-de-dados.ipynb')
+with col2:
+    st.image('img/obesidity.jpg',width=250)
