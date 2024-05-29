aeb1fd4bbfb5e3d0ed4f69d92fc449a4f3b75c99
Geovani Lima <122645609+geovani2311@users.noreply.github.com>
2024-05-29 00:16:22 -0300

    Update 📩_Contacts.py

--- pages/📩_Contacts.py
+++ pages/📩_Contacts.py
@@ -1,7 +1,6 @@
 import streamlit as st
 
 st.header("Contacts 📩", divider='rainbow')
-st.header("")
 col1, col2, col3, col4, col5 = st.columns(5)
 with col1:
     st.link_button("Linkedin", "https://www.linkedin.com/in/geovani-lima-cardoso-760212158/")
